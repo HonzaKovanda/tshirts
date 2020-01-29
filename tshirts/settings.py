53d5904710ab546b2b18fbdead62e0d2428a5b23
Honza Kovanda <kovandah@gmail.com>
2020-01-29 15:21:14 +0100

    Login and signup 2

--- tshirts/settings.py
+++ tshirts/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^uu01plsncew=wb#1-7(fhg-^@ej^a71%ng1%753y!7g61cc#m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'honzik.pythonanywhere.com']
 
 
 # Application definition
