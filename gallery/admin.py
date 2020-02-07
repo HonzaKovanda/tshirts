9b7763daa2855857e81a676d3af96becee2fb2dc
Honza Kovanda <kovandah@gmail.com>
2020-02-07 22:10:23 +0100

    Position in admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Size, Image
+from .models import Size, Image, Position
 
 admin.site.register(Size)
 admin.site.register(Image)
+admin.site.register(Position)
