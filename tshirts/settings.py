32014b912c6cda67181cfa12d9af87f7a0b0c2b7
Honza Kovanda <kovandah@gmail.com>
2020-02-14 12:40:15 +0100

    S3 settings

--- tshirts/settings.py
+++ tshirts/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'gallery',
     'model_clone',
     'crispy_forms',
+    'storages',
     'django.contrib.humanize',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -132,6 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+"""
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
@@ -145,6 +147,28 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+"""
+
+
+#set S3 as the place to store your files.
+DEFAULT_FILE_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
+STATICFILES_STORAGE = “storages.backends.s3boto3.S3Boto3Storage”
+AWS_ACCESS_KEY_ID = os.environ.get(“AWS_ACCESS_KEY_ID”, “”)
+AWS_SECRET_ACCESS_KEY = os.environ.get(“AWS_SECRET_ACCESS_KEY”, “”)
+AWS_STORAGE_BUCKET_NAME = os.environ.get(“AWS_STORAGE_BUCKET_NAME”, “”)
+AWS_QUERYSTRING_AUTH = False #This will make sure that the file URL does not have unnecessary parameters like your access key.
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com’
+#static media settings
+STATIC_URL = ‘https://' + AWS_STORAGE_BUCKET_NAME + ‘.s3.amazonaws.com/’
+MEDIA_URL = STATIC_URL + ‘media/’
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, “static”), )
+STATIC_ROOT = ‘staticfiles’
+ADMIN_MEDIA_PREFIX = STATIC_URL + ‘admin/’
+STATICFILES_FINDERS = (
+‘django.contrib.staticfiles.finders.FileSystemFinder’,
+‘django.contrib.staticfiles.finders.AppDirectoriesFinder’,
+)
+
 
 
 LOGIN_REDIRECT_URL = '/kosik/migrate_temp_user/'
