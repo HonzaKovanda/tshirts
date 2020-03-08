693a466af534db9c43d85551242245915ad8a1bd
Honza Kovanda <kovandah@gmail.com>
2020-03-08 19:55:57 +0100

    Products tests

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,52 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+from decouple import config
+import requests
+from django.urls import reverse
+
+import tempfile
+
+from .apps import get_price_with_tax
+from .adler_stock import load_stock, stock_status
+from .models import Color, Tshirt, Category, Supplier
+
+
+class ProductsTests (TestCase):
+
+    def test_get_price_with_tax(self):
+        DPH = 1.21
+        assert get_price_with_tax(100) == (100*DPH)
+    
+    def test_load_stock(self):
+        r = requests.get(config('ADLER_JSON'))
+        data = r.json()
+        assert load_stock() == data
+
+    def setUp(self):
+        Color.objects.create(title='Černá')
+
+    def test_color_title(self):
+        color = Color.objects.get(id=1)
+        expected_object_name = f'{color.title}'
+        self.assertEquals(expected_object_name, 'Černá')
+
+    def test_products_home_view(self):
+        response = self.client.get(reverse('products:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'products/index.html')
+
+    def test_products_detail_view(self):
+        category = Category.objects.create()
+        Supplier.objects.create()
+        color = Color.objects.get(id=1)
+        image = tempfile.NamedTemporaryFile(suffix=".jpg").name
+
+        tshirt = Tshirt.objects.create(title='Pure', slug='pure-cerna', price=200, category=category, 
+        nomen_code="1220115", color=color, image=image,)
+
+        response = self.client.get(reverse('products:detail', args=(tshirt.slug,)))
+        self.assertEqual(response.status_code, 200) 
+        self.assertTemplateUsed(response, 'products/detail.html')
+
+
--- tshirts/settings.py
+++ tshirts/settings.py
@@ -174,30 +174,6 @@ STATICFILES_FINDERS = (
 )
 
 
-
-
-#set S3 as the place to store your files.
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-AWS_QUERYSTRING_AUTH = False #This will make sure that the file URL does not have unnecessary parameters like your access key.
-AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com'
-AWS_DEFAULT_ACL = None
-#static media settings
-STATIC_URL = 'https://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
-MEDIA_URL = STATIC_URL + 'media/'
-MEDIA_ROOT = 'mediafiles'
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
-STATIC_ROOT = 'staticfiles'
-ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-STATICFILES_FINDERS = (
-'django.contrib.staticfiles.finders.FileSystemFinder',
-'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
-
-
 LOGIN_REDIRECT_URL = '/kosik/migrate_temp_user/'
 LOGOUT_REDIRECT_URL = '/'
 
